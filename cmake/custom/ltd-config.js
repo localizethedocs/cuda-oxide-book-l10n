ac8545509226cfe9f15591afd99039f21b4bee47
Haowei Hsu <hwhsu1231@localizethedocs.org>
2026-07-16 17:57:03 +0800

    ✨ feat(init): initialize branch 'main'
    Initialize the 'main' branch with the following files:
    
    - Add document files: README.md, LICENSE-BSD, LICENSE-CC.
    - Add GitHub workflow files: ci-check-pr-status,..., etc.
    - Add GitHub issue templates: language_request.yml,..., etc.
    - Add Git config files: .gitmodules, .gitignore.
    - Add Git submodules: cmake/modules, cmake/plugins.
    - Add CMake script files: CMakeLists.txt, cmake/**/* files.
    - Add config files: versions.json, languages.json, crowdin.yml.

---
+++ cmake/custom/ltd-config.js
@@ -0,0 +1,19 @@
+"use strict";
+
+var CONFIG_OPTIONS = {
+  CONFIG_LANGUAGES: [
+    ["en-us",   "English"],
+    ["zh-cn",   "简体中文"],
+    ["zh-tw",   "繁體中文"],
+  ],
+  CONFIG_VERSIONS: [
+    ["main",    "Development"],
+  ],
+  CONFIG_PROJECTS: [
+    ["Index",   "https://projects.localizethedocs.org"],
+    ["Crowdin", "https://localizethedocs.crowdin.com/cuda-oxide-book-l10n"],
+    ["GitHub",  "https://github.com/localizethedocs/cuda-oxide-book-l10n"],
+    ["AtomGit", "https://atomgit.com/localizethedocs/cuda-oxide-book-l10n"],
+    ["GitFlic", "https://gitflic.ru/project/localizethedocs/cuda-oxide-book-l10n"],
+  ]
+};
